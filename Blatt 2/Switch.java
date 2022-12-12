ba1bbce84058ff0c4a239281b829e7e296d88671
Tela Mangoua <h.tela-mangoua@tu-braunschweig.de>
2022-12-12 13:50:31 +0100

    the first existing code

---
+++ Blatt 2/Switch.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Switch {
+
+    public static void main(String[] args) {
+
+        Scanner myWord = new Scanner(System.in);
+        System.out.println("enter the word to define");
+        String word = myWord.nextLine();
+        switch(word) {
+            case "hoch":
+                System.out.println("The opposite is: tief");
+                break;
+            case "tief":
+                System.out.println("The opposite is: hoch");
+                break;
+            case "klein":
+                System.out.println("The opposite is: groß");
+                break;
+            case "groß":
+                System.out.println("The opposite is: klein");
+                break;
+            case "stark":
+                System.out.println("The opposite is: schwach");
+                break;   
+            case "schwach":
+                System.out.println("The opposite is: stark");
+                break; 
+            default:
+                System.out.println("the Word was not define");
+        }
+        //myWord.close();
+    }
+}
