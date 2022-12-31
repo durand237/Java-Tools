2946f9b4c7b0f166d813e2960f24bda633527c4c
Tela Mangoua <h.tela-mangoua@tu-braunschweig.de>
2022-12-31 11:51:24 +0100

    create new classes

---
+++ Zoo Simulation/Zoo.java
@@ -0,0 +1,16 @@
+
+public class Zoo {
+    private double food = 0;
+    private double drink = 0;
+    private final int numberOfCasten = 0;
+    private Tier casten;
+    public Zoo(int essen, int wasser, int gehegeAnzahl) {
+        food = essen;
+        drink = wasser;
+        numberOfCasten = gehegeAnzahl;
+    }
+
+    public void neuesTier(Tier tier) {
+
+    }
+}
