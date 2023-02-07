9957b7a572606ef5e264c06da69ffdbbb4108855
Tela Mangoua <h.tela-mangoua@tu-braunschweig.de>
2023-02-07 10:26:54 +0100

    Project X

--- Java App/app/src/main/java/com/example/myhomecm/MainActivity.java
+++ Java App/app/src/main/java/com/example/myhomecm/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myhomecm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -23,10 +24,18 @@ public class MainActivity extends AppCompatActivity {
         button.setText("goodbye see you later");
         Log.d("succes", "Button connected");
     }*/
-    public void handleText(View v) {
+    /*public void handleText(View v) {
         EditText txt = findViewById(R.id.search);
         String input = txt.getText().toString();
+        Log.d("myLog", "message");
         ((TextView)findViewById(R.id.output)).setText(input);
-        Toast.makeText(this, "attention")
+        Toast.makeText(this, "attention", Toast.LENGTH_LONG).show();
+
+    }*/
+    //intent
+    public void launchActivities(View v) {
+        //launch a new activity
+        Intent i = new Intent(this, setting.class);
+        startActivity(i);
     }
 }
